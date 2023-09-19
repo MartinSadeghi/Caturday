38125229144819f1f0ccd317823711d4d8ce55a5
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-09-19 14:32:44 +0200

    fetchAllBreeds added in Network.

--- Network/BreedFetcher.swift
+++ Network/BreedFetcher.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+
+class BreedFetcher: ObservableObject {
+    
+    var breeds = [Breed]()
+    var errorMessage: String? = nil
+    var isLoading = false
+    
+    init() {
+        
+    }
+    
+    func fetchAllBreeds() {
+        isLoading = true
+        guard let url = URL(string: "https://api.thecatapi.com/v1/breeds?limit=50") else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            self?.isLoading = false
+            let decoder = JSONDecoder()
+            if let data = data {
+                do {
+                    let breeds = try decoder.decode([Breed].self, from: data)
+                    self?.breeds = breeds
+                    print(breeds)
+                } catch {
+                    print("data can not be decoded")
+                }
+            }
+        }
+        
+    }
+}
