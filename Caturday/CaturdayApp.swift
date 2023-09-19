e4a88a0d7f1e7f0a209a6774fc19ac55e02f693b
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-09-19 12:00:16 +0200

    Initial Commit

---
+++ Caturday/CaturdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaturdayApp.swift
+//  Caturday
+//
+//  Created by Mommo Sadeghi on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CaturdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
