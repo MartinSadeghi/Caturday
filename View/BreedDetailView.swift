5ba28f4b7f07a40dda1b87fd2c670f1a1962bc8c
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-09-22 11:55:39 +0200

    BreedDetailView VStack and HStack added.

--- View/BreedDetailView.swift
+++ View/BreedDetailView.swift
@@ -11,6 +11,7 @@ struct BreedDetailView: View {
     let breed: Breed
     
     let imageSize: CGFloat = 300
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -19,9 +20,11 @@ struct BreedDetailView: View {
                         if let image = phase.image {
                             image.resizable()
                                 .scaledToFit()
-                                .frame(height: imageSize)
+                                .frame( height: imageSize)
                                 .clipped()
+                            
                         } else if phase.error != nil {
+                            
                             Text(phase.error?.localizedDescription ?? "error")
                                 .foregroundColor(Color.pink)
                                 .frame(width: imageSize, height: imageSize)
@@ -31,34 +34,86 @@ struct BreedDetailView: View {
                         }
                         
                     }
-                } else {
+                }else {
                     Color.gray.frame(height: imageSize)
                 }
                 
-                VStack(alignment: .leading, spacing: 5) {
-                        Text(breed.name!)
-                            .font(.headline)
-                        Text(breed.temperament!)
-                            .font(.footnote)
-                        Text(breed.description!)
-                        if breed.hairless {
-                            Text("Hairless")
+                VStack(alignment: .leading, spacing: 15) {
+                    
+                    Text(breed.name ?? "")
+                        .font(.headline)
+                    Text(breed.temperament ?? "")
+                        .font(.footnote)
+                    //                                        Text(breed.breedExplaination)
+                    if breed.hairless {
+                        Text("hairless")
+                    }
+                    
+                    HStack {
+                        Text("Energy level")
+                        Spacer()
+                        ForEach(1..<6) { id in
+                            Image(systemName: "star.fill")
+                                .foregroundColor(breed.energy_level ?? 1 > id ? Color.accentColor : Color.gray )
                         }
                     }
-                        HStack {
-                            Text("Energy level")
-                            Spacer()
-                            ForEach (1..<6) { id in
-                                Image(systemName: "star.fill")
-                                    .foregroundColor((breed.energy_level!) > id ? Color.accentColor : Color.gray)
-                            }
-                    }
+                    
                     Spacer()
                 }.padding()
-                navigationBarTitleDisplayMode(.inline)
+                    .navigationBarTitleDisplayMode(.inline)
             }
         }
     }
+}
+//    var body: some View {
+//        ScrollView {
+//            VStack {
+//                if breed.image?.url != nil {
+//                    AsyncImage(url: URL(string: breed.image!.url!)) { phase in
+//                        if let image = phase.image {
+//                            image.resizable()
+//                                .scaledToFit()
+//                                .frame(height: imageSize)
+//                                .clipped()
+//                        } else if phase.error != nil {
+//                            Text(phase.error?.localizedDescription ?? "error")
+//                                .foregroundColor(Color.pink)
+//                                .frame(width: imageSize, height: imageSize)
+//                        } else {
+//                            ProgressView()
+//                                .frame(width: imageSize, height: imageSize)
+//                        }
+//
+//                    }
+//                } else {
+//                    Color.gray.frame(height: imageSize)
+//                }
+//            }
+//            VStack(alignment: .leading, spacing: 5) {
+//                Text(breed.name!)
+//                    .font(.headline)
+//                Text(breed.temperament!)
+//                    .font(.footnote)
+//                Text(breed.description!)
+//                if breed.hairless {
+//                    Text("Hairless")
+//                }
+//            }
+//        }
+//                        HStack {
+//                            Text("Energy level")
+//                            Spacer()
+//                            ForEach (1..<6) { id in
+//                                Image(systemName: "star.fill")
+//                                    .foregroundColor((breed.energy_level!) > id ? Color.accentColor : Color.gray)
+//                            }
+//                            Spacer()
+//                    }.padding()
+//        navigationBarTitleDisplayMode(.inline)
+//
+//                }
+//            }
+
 
 
 struct BreedDetailView_Previews: PreviewProvider {
