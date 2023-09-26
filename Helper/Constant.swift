56d52d6a0111f3bedff46714d7f2520205d0f526
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-09-26 11:36:41 +0200

    Constant file added for saving the KEYS and String values.

---
+++ Helper/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  Caturday
+//
+//  Created by Mommo Sadeghi on 26/09/23.
+//
+
+import Foundation
+
+
+enum Constanat {
+    
+    static let APIKEY = "https://api.thecatapi.com/v1/breeds?api_key=live_XKtBd9LQilC4ztESKW4khbnb8O5exF3fzVeSBlb6hWnzp24L2ept2qvDmEH1NaiM"
+}
